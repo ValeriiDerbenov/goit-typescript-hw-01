260dcf68970010b7923f8fa9807b4c98eb0a3bcc
Valerii <vd.alternative@gmail.com>
2024-02-20 23:09:41 +0200

    type

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,3 @@
-// import { concatenation } from './concatenation';
 import { concatenation } from "./concatenation";
 const button = document.querySelector('button');
 const input = document.querySelector('input');
@@ -7,4 +6,28 @@ if (button && input) {
         concatenation(input.value, 'hello!');
     });
 }
+let total = 100;
+let name = "Valerii";
+console.log(total, name);
+let isActive = false;
+console.log(isActive);
+let empty = null;
+let empty2 = undefined;
+name = "5";
+const arr = [1, 2, 3];
+const arr2 = [1, 2, 3];
+arr2.push(4);
+const user = {
+    name: "Valerii",
+    age: 20,
+};
+const user2 = {
+    name: "Valerii",
+    age: 20,
+};
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(1, 4));
+console.log(user);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-// import { concatenation } from './concatenation';
-
 import { concatenation } from "./concatenation";
 
 const button = document.querySelector('button')!;
@@ -10,3 +8,35 @@ if (button && input) {
     concatenation(input.value, 'hello!');
   });
 }
+
+let total: number = 100;
+let name: string = "Valerii";
+console.log(total, name);
+let isActive: boolean = false;
+console.log(isActive);
+let empty: null = null;
+let empty2: undefined = undefined;
+name = "5"
+const arr: number[] = [1, 2, 3];
+const arr2: Array<number> = [1, 2, 3];
+arr2.push(4);
+
+const user: { name: string; age: number } = {
+  name: "Valerii",
+  age: 20,
+};
+type User = {
+  name: string;
+  age: number;
+};
+const user2: User = {
+  name: "Valerii",
+  age: 20,
+}
+
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+console.log(add(1, 4));
+console.log(user);
