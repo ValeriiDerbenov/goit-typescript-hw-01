c256fd016d75428043cdb890eb53045dc0123f48
Valerii <vd.alternative@gmail.com>
2024-02-21 09:05:09 +0200

    learned

--- dist/index.js
+++ dist/index.js
@@ -6,28 +6,66 @@ if (button && input) {
         concatenation(input.value, 'hello!');
     });
 }
-let total = 100;
-let name = "Valerii";
-console.log(total, name);
-let isActive = false;
-console.log(isActive);
-let empty = null;
-let empty2 = undefined;
-name = "5";
-const arr = [1, 2, 3];
-const arr2 = [1, 2, 3];
-arr2.push(4);
-const user = {
-    name: "Valerii",
-    age: 20,
-};
-const user2 = {
-    name: "Valerii",
-    age: 20,
-};
+// let total: number = 100;
+// let name: string = "Valerii";
+// console.log(total, name);
+// let isActive: boolean = false;
+// console.log(isActive);
+// let empty: null = null;
+// let empty2: undefined = undefined;
+// name = "5"
+// const arr: number[] = [1, 2, 3];
+// const arr2: Array<number> = [1, 2, 3];
+// arr2.push(4);
+// const user: { name: string; age: number } = {
+//   name: "Valerii",
+//   age: 20,
+// };
+// type User = {
+//   name: string;
+//   age: number | string;
+// };
+let name1 = "Val";
+if (typeof name1 === "string") {
+    // console.log(name1);
+}
+name1.toUpperCase();
+const event = "homework";
+var Size;
+(function (Size) {
+    Size["Small"] = "S";
+    Size["Medium"] = "M";
+    Size["Large"] = "L";
+})(Size || (Size = {}));
+// console.log(Size.Small);
+const size = Size.Large;
+// console.log(size);
 function add(num1, num2) {
     return num1 + num2;
 }
-console.log(add(1, 4));
-console.log(user);
+// console.log('add :>> ', add(1, 4));
+function sub(num1, num2) {
+    console.log(num1 - num2);
+}
+// sub(1, 4);
+// sub(4, 1);
+function mult(num1, num2) {
+    return num1 * num2;
+}
+function greet(user) {
+    console.log('hello ' + user.name);
+}
+greet({ name: 'Bob' });
+function greet2(name, age) {
+    return {
+        name,
+        age
+    };
+}
+console.log('greet2( "Valerii", 20 ) :>> ', greet2('Valerii', 20));
+function startCar(car) {
+    console.log('Started ' + car.name);
+    car.start(car.color);
+}
+startCar({ name: 'BMW', price: 10000, color: 'red', start: (color) => console.log(color) });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -9,34 +9,99 @@ if (button && input) {
   });
 }
 
-let total: number = 100;
-let name: string = "Valerii";
-console.log(total, name);
-let isActive: boolean = false;
-console.log(isActive);
-let empty: null = null;
-let empty2: undefined = undefined;
-name = "5"
-const arr: number[] = [1, 2, 3];
-const arr2: Array<number> = [1, 2, 3];
-arr2.push(4);
+// let total: number = 100;
+// let name: string = "Valerii";
+// console.log(total, name);
+// let isActive: boolean = false;
+// console.log(isActive);
+// let empty: null = null;
+// let empty2: undefined = undefined;
+// name = "5"
+// const arr: number[] = [1, 2, 3];
+// const arr2: Array<number> = [1, 2, 3];
+// arr2.push(4);
 
-const user: { name: string; age: number } = {
-  name: "Valerii",
-  age: 20,
-};
-type User = {
-  name: string;
-  age: number;
-};
-const user2: User = {
-  name: "Valerii",
-  age: 20,
+// const user: { name: string; age: number } = {
+//   name: "Valerii",
+//   age: 20,
+// };
+// type User = {
+//   name: string;
+//   age: number | string;
+// };
+
+
+let name1: string = "Val";
+if (typeof name1 === "string") {
+  // console.log(name1);
 }
+name1.toUpperCase();
+// console.log('name1 :>> ', name1);
 
-function add(num1: number, num2: number) {
+// console.log(add(1, 4));
+
+
+type eventType = "lesson" | "homework";
+const event: eventType = "homework";
+
+enum Size {
+  Small = "S",
+  Medium = "M",
+  Large = "L",
+}
+// console.log(Size.Small);
+const size: Size = Size.Large;
+// console.log(size);
+
+
+function add(num1: number, num2: number): number {
   return num1 + num2;
 }
+// console.log('add :>> ', add(1, 4));
 
-console.log(add(1, 4));
-console.log(user);
+function sub(num1: number, num2: number): void {
+  console.log(num1 - num2);
+}
+// sub(1, 4);
+// sub(4, 1);
+
+
+
+function mult(num1: number, num2: number) {
+  return num1 * num2;
+}
+
+type User3 = {
+  name: string;
+}
+function greet(user: User3): void {
+  console.log('hello ' + user.name);
+}
+greet({ name: 'Bob' });
+
+type User4 = {
+  name: string;
+  age: number;
+}
+function greet2(name: string, age: number ): User4 {
+  return {
+    name,
+    age
+  }
+}
+
+console.log('greet2( "Valerii", 20 ) :>> ', greet2( 'Valerii', 20 ));
+
+type Car = {
+  name: string;
+  price: number;
+  color: string;
+  start: (color: string) => (void);
+};
+
+function startCar(car: Car) {
+  console.log('Started ' + car.name);
+  car.start(car.color);
+}
+
+startCar({ name: 'BMW', price: 10000, color: 'red', start: (color) => console.log(color) });
